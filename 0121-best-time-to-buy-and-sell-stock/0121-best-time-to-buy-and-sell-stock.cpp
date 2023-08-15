df448ebca426ced073706765546a92e3b786d567
clemcheyrou <75241945+clemcheyrou@users.noreply.github.com>
2023-08-15 15:35:32 +0200

    Time: 107 ms (73.97%), Space: 93.3 MB (78.55%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,20 +1,19 @@
-class Solution {
-public:
-    int maxProfit(vector<int>& prices)
-    {
-        int left = 0;
-        int right = 1;
-        int max = 0;
-        while (right < prices.size())
-        {
-            int current = prices[right] - prices[left];
-            if (prices[left] < prices[right])
-                max = std::max(current , max);
-            else
-                left = right;
-            right++;
-        }
-        return (max);
-    }
+class Solution
+{
+    public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+        int buyIndex = 0;
+        int numPrices = prices.size();
 
+        for (int sellIndex = 1; sellIndex < numPrices; ++sellIndex) {
+            int currentProfit = prices[sellIndex] - prices[buyIndex];
+            if (currentProfit > 0)
+                maxProfit = max(maxProfit, currentProfit);
+            else
+                buyIndex = sellIndex;
+        }
+        
+        return (maxProfit);
+    }
 };
