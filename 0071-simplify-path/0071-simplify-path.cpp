1b594e4c36fd7bb7291172a48def4a27a435101d
clemcheyrou <75241945+clemcheyrou@users.noreply.github.com>
2023-09-26 15:24:01 +0200

    Time: 3 ms (90.85%), Space: 9.5 MB (17.49%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,40 +1,29 @@
+
 class Solution {
 public:
     string simplifyPath(string path) {
-        
-        stack<string> st;
-        string res;
-        
-        for(int i = 0; i < path.size(); ++i)
-        {
-            if (path[i] == '/')    
-                continue;
-            string temp;
-            while(i < path.size() && path[i] != '/')
-            {
-                temp += path[i];
-                ++i;
+        stack<string> str;
+
+        stringstream ss(path);
+        string el;
+
+        while (getline(ss, el, '/')) {
+            if (el == "..") {
+                if (!str.empty()) {
+                    str.pop();
+                }
+            } else if (el != "." && !el.empty()) {
+                str.push(el);
             }
-            if (temp == ".")
-                continue;
-            else if (temp == "..")
-            {
-                if (!st.empty())
-                    st.pop();
-            }
-            else
-                st.push(temp);
         }
-        
-        while (!st.empty())
-        {
-            res = "/" + st.top() + res;
-            st.pop();
+
+        // Reconstruct the simplified path
+        string simplifiedPath = "";
+        while (!str.empty()) {
+            simplifiedPath = "/" + str.top() + simplifiedPath;
+            str.pop();
         }
-        
-        if(res.size() == 0)
-            return "/";
-        
-        return res;
+
+        return simplifiedPath.empty() ? "/" : simplifiedPath;
     }
 };
